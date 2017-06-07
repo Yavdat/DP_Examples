1958ec8b849a4ed6b632d9feff3349dde74c9ddc
Yavdat <yavdat92@mail.ru>
2017-06-08 00:57:45 +0300

    +some refactoring Strategy

--- Strategy2.php
+++ Strategy2.php
@@ -4,82 +4,75 @@ abstract class Question {
     protected $prompt;
     protected $marker;
 
-    function __construct( $prompt, Marker $marker )
-    {
-        $this->marker = $marker;
-        $this->marker = $prompt;
+    function __construct( $prompt, Marker $marker ) {
+        $this->marker=$marker;
+        $this->prompt = $prompt;
     }
 
-    function mark( $response )
-    {
-        return $this->marker->mark($response);
+    function mark( $response ) {
+        return $this->marker->mark( $response );
     }
 }
 
 class TextQuestion extends Question {
-    //Выполняются действия, специфичные для текстовых вопросов
+// do text question specific things
 }
 
 class AVQuestion extends Question {
-    //Выполняются действия, специфичные для мультимедийных вопросов
+// do audiovisual question specific things
 }
 
 abstract class Marker {
     protected $test;
 
-    function __construct( $test )
-    {
+    function __construct( $test ) {
         $this->test = $test;
     }
 
-    abstract function mark($response);
+    abstract function mark( $response );
 }
 
 class MarkLogicMarker extends Marker {
-
     private $engine;
-
-    function __construct($test)
-    {
-        parent::__construct($test);
-//        $this->engine = new MarkerParse($test);
+    function __construct( $test ) {
+        parent::__construct( $test );
+// $this->engine = new MarkParse( $test );
     }
 
-    function mark($response) {
-//        return $this->engine->evaluate($response);
-        //Возвратим фиктивное значение;
+    function mark( $response ) {
+// return $this->engine->evaluate( $response );
+// dummy return value
         return true;
     }
 }
 
 class MatchMarker extends Marker {
-    function mark($response){
+    function mark( $response ) {
         return ( $this->test == $response );
     }
 }
 
 class RegexpMarker extends Marker {
     function mark( $response ) {
-        return (preg_match($this->test, $response));
+        return ( preg_match( $this->test, $response ) );
     }
 }
 
-$markers = array(new RegexpMarker("/П.ть/"),
-    new MatchMarker("Marker"),
-    new MarkLogicMarker('$input equals "Пять"')
+$markers = array(
+    new RegexpMarker( "/f.ve/" ),
+    new MatchMarker( "five" ),
+    new MarkLogicMarker( '$input equals "five"' )
 );
 
-foreach ($markers as $marker) {
-    print get_class($marker)."\n";
-    $question = new TextQuestion("Сколько лучей к Кремлевской звезды?", $marker);
-
-    foreach ( array("Пять", "Четыре") as $response ) {
-        print "Ответ: $response: ";
-        if ($question->mark($response)) {
-            print "Правильно! \n";
+foreach ( $markers as $marker ) {
+    print get_class( $marker )."\n";
+    $question = new TextQuestion( "how many beans make five", $marker );
+    foreach ( array( "five", "four" ) as $response ) {
+        print "\tresponse: $response: ";
+        if ( $question->mark( $response ) ) {
+            print "well done\n";
         } else {
-            print "Неверно! \n";
+            print "never mind\n";
         }
     }
 }
-
